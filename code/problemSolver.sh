6f5d5e4855b6953f53ed3d5fb87c1a17bd3e82b7
Ying Xian Wu <u6958577@anu.edu.au>
2022-05-13 11:54:17 +0800

    Ignore temporary files

--- code/problemSolver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#author: Pascal Bercher (pascal.bercher@anu.edu.au)
-
-./pandaPIparser/pandaPIparser $1 $2 parsed/problem$3.parsed > parsed/problem$3.parsed.log
-./pandaPIgrounder/pandaPIgrounder parsed/problem$3.parsed parsed/problem$3.sas 2> parsed/problem$3.stderr.statistics > parsed/problem$3.stdout.statistics
-./pandaPIengine/build/pandaPIengine parsed/problem$2.sas > parsed/problem$3.solution
